5fb44252cd44b589cd02e68b5f91c631f0b7c3ac
Santiago Ruiz <hiebra@hotmail.com>
2022-02-16 14:33:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const wadl = require('./wadl/utilities.js')
                      
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const path = core.getInput('path');
+  console.log(`Procesando el archivo fuente WADL de la ruta ${path}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
