273fca3339b18c7154d6a7bc84ecaa2f38974164
Santiago Ruiz <hiebra@hotmail.com>
2022-02-16 16:56:32 +0100

    Update utilities.js

--- common/utilities.js
+++ common/utilities.js
@@ -1,7 +1,10 @@
-const { run } = require("child_process");
+const { run } = require("child_process")
+const fs = require('fs')
 
 module.exports = {
   exec: function(command) {
+    console.log(fs)
+    console.log(run)
     run("ls -la", (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`)
