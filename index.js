cce13d267441facd876dbb047d1f5d4f4f437966
Santiago Ruiz <hiebra@hotmail.com>
2022-02-16 14:39:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const java = require('./java/generate.js')
 const wadl = require('./wadl/utilities.js')
                      
 try {
-  // `who-to-greet` input defined in action metadata file
   const path = core.getInput('path');
-  console.log(`Procesando el archivo fuente WADL de la ruta ${path}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  wadl.log(path, github.context.payload.head_commit.message.startsWith("Created"))
+  wadl.log(path, github.context.payload.head_commit.message.startsWith("Update"))
   java.log()
   console.log(`The event payload: ${payload}`);
 } catch (error) {
