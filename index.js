0b6a3798924ef690c7dfc8e9d9dfb32a495735ba
Santiago Ruiz <hiebra@hotmail.com>
2022-02-16 14:05:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const java = require('./java/generate.js')
+const wadl = require('./wadk/utilities.js')
                      
 try {
   // `who-to-greet` input defined in action metadata file
@@ -10,6 +11,7 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  wadl.log()
   java.log()
   console.log(`The event payload: ${payload}`);
 } catch (error) {
