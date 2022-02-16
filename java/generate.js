4feb460c13afa7b9cb10b68aaa48fea086962f63
Santiago Ruiz <hiebra@hotmail.com>
2022-02-16 13:39:46 +0100

    Update generate.js

--- java/generate.js
+++ java/generate.js
@@ -1,8 +1,8 @@
 module.exports = {
   log: function () {
     const fs = require('fs')
-    const content = 'Some content!'
-    fs.writeFile('/Users/joe/test.txt', content, err => {
+    const content = 'Hello, World!'
+    fs.writeFile('hello-world.txt', content, err => {
       if (err) {
         console.error(err)
       } else {
