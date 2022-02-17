18856ee383753f1b964ed7000229709dcba0a730
Santiago Ruiz <hiebra@hotmail.com>
2022-02-17 01:29:12 +0100

    Update Build.java

--- src/main/java/es/aeat/wadl/Build.java
+++ src/main/java/es/aeat/wadl/Build.java
@@ -5,6 +5,6 @@ import java.util.logging.Logger;
 public class Build {
   public static void main(String[] args) {
     System.err.println("Hello, World!");
-    Logger.getGlobal().servere("Hello, World!");
+    Logger.getGlobal().severe("Hello, World!");
   }
 }
